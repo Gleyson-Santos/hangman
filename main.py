da381dd16335dede7f5f97dad1a93c05363a1d0a
Gleyson Santos <gleysonsantos@protonmail.com>
2022-11-02 13:28:06 -0300

    checking if the player has won

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import random
 
 # Step 1
 # Step 2
+# Checking if the player has won
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+word_length = len(chosen_word)
 display = []
-for _ in chosen_word:
+for _ in range(word_length):
     display.append('*')
 print(display)
 
-guess = input('Guess a letter: ').lower()
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+player_has_won = False
+while not player_has_won:
+    guess = input('Guess a letter: ').lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if '*' not in display:
+        player_has_won = True
 
-print(display)
+    print(display)
