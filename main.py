c4492b6b460d73132c30b16d942dd1f1fcda9138
Gleyson Santos <gleysonsantos@protonmail.com>
2022-11-02 13:07:50 -0300

    lowering the guesses with the lower() function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-guess = input('Guess a letter: ')
+guess = input('Guess a letter: ').lower()
 for letter in chosen_word:
     if letter == guess:
         print('Right')
